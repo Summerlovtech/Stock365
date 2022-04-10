bcfd694668abd5c90f7ed08325bf83fef0001213
Summerlovtech <lynnenjoylife@outlook.com>
2022-04-10 10:25:57 -0700

    Initial Commit

---
+++ Stock365/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock365
+//
+//  Created by Summer Lin on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
